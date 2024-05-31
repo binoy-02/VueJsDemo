13980bbe16a9773b849ff3d358d7275db3c74d26
binoy-02 <bnyjolly@gmail.com>
2024-06-01 01:07:42 +0530

    class conditional binds

--- hello_world/src/App.vue
+++ hello_world/src/App.vue
@@ -2,14 +2,16 @@
   <TextBind></TextBind>
   <HtmlBind></HtmlBind>
   <DataBind></DataBind>
+  <ClassDemo></ClassDemo>
 </template>
 
 <script>
 import TextBind from './components/TextBind.vue'
 import HtmlBind from './components/HtmlBind.vue'
 import DataBind from './components/DataBind.vue'
+import ClassDemo from './components/ClassDemo.vue'
 export default {
-  components : {TextBind,HtmlBind,DataBind },
+  components : {TextBind,HtmlBind,DataBind, ClassDemo },
   
 }
 </script>
--- hello_world/src/components/ClassDemo.vue
+++ hello_world/src/components/ClassDemo.vue
@@ -1,7 +1,20 @@
 <template>
     <h2>ClassBind</h2>
     <h2 class="Underline">Underlined Text</h2>  
-    <h2 v-bind:class="status">Status</h2>
+    <h2 class="Underline" v-bind:class="status">Status</h2>
+    <h2 v-bind:class="isPromoted && 'PromotedMovie'" >Promoted Movie</h2>
+    <h2 v-bind:class="isNew && 'NewMovie'">New Movie</h2>
+    <h2 v-bind:class="isSoldout ? 'SoldOut':'NewMovie'">SoldOut Movie</h2>
+    <h2 v-bind:class="[isPromoted&&'NewMovie',isNew&&'PromotedMovie']">New Promoted Movie</h2>
+    <h2 v-bind:class="[isPromoted && 'PromotedMovie',isSoldout?'SoldOut':'NewMovie']">Conditional Array Movie</h2>
+    <h2 v-bind:class="{'NewMovie' : !SoldOut,
+        'PromotedMovie' : isPromoted,
+        'SoldOut' : SoldOut,}
+        ">
+      Object conditional movie
+    </h2>
+    
+    
   </template>
   
   <script>
@@ -11,6 +24,9 @@
        headingID : 'heading',
        isDisabled : true,
        status: 'danger',
+       isPromoted : false,
+       isNew : true,
+       isSoldout : false,
     }
     }
 
@@ -30,6 +46,18 @@
   .Underline{
     text-decoration: underline;
     
+  }
+
+  .PromotedMovie{
+   font-style :italic;
+  }
+
+  .NewMovie{
+    color: green;
+  }
+  .SoldOut{
+    color: red;
+    position:bottom;
   }
   </style>
   
